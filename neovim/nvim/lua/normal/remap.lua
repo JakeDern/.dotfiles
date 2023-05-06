ac15d730361813a165f6ba2ed7cf0e62c1dceaef
Jake Dern <jakedern@yahoo.com>
2023-05-06 15:32:35 -0700

    improvements

--- neovim/nvim/lua/normal/remap.lua
+++ neovim/nvim/lua/normal/remap.lua
@@ -2,3 +2,6 @@ vim.keymap.set("n", "<leader>w", vim.cmd.Ex)
 
 vim.keymap.set("n", "<leader>u", ":UndertreeShow<CR>")
 
+vim.keymap.set("n", "<leader>f", function()
+    vim.lsp.buf.format()
+end)
--- neovim/nvim/lua/vscode_lua/remap.lua
+++ neovim/nvim/lua/vscode_lua/remap.lua
@@ -1,14 +1,16 @@
-
+-- Misc
 vim.keymap.set("n", "<leader>pp", "<cmd>call VSCodeNotifyVisual('workbench.action.showCommands', 1)<CR>")
 vim.keymap.set("n", "<leader>w", "<cmd>call VSCodeNotify('workbench.action.closeActiveEditor')<CR>")
 vim.keymap.set("n", "<leader>g", "<cmd>call VSCodeNotify('workbench.action.quickOpen')<CR>")
 vim.keymap.set("n", "<leader>ps", "<cmd>call VSCodeNotify('workbench.action.findInFiles')<CR>")
 vim.keymap.set("n", "<leader>pr", "<cmd>call VSCodeNotify('workbench.action.replaceInFiles')<CR>")
 
+-- Quick swap editior windows
 vim.keymap.set("n", "<C-h>", "<cmd>call VSCodeNotify('workbench.action.openEditorAtIndex1')<CR>")
 vim.keymap.set("n", "<C-j>", "<cmd>call VSCodeNotify('workbench.action.openEditorAtIndex2')<CR>")
 vim.keymap.set("n", "<C-k>", "<cmd>call VSCodeNotify('workbench.action.openEditorAtIndex3')<CR>")
 vim.keymap.set("n", "<C-l>", "<cmd>call VSCodeNotify('workbench.action.openEditorAtIndex4')<CR>")
 
+-- Swap windows left or right
 vim.keymap.set("n", "<leader>fl", "<cmd>call VSCodeNotify('workbench.action.nextEditor')<CR>")
 vim.keymap.set("n", "<leader>fh", "<cmd>call VSCodeNotify('workbench.action.previousEditor')<CR>")
