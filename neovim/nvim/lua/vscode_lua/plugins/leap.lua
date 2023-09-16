7b3e2c3e0162b5c79f98fcf152249a5faccef796
Jake Dern <jakedern@yahoo.com>
2023-09-16 15:14:27 -0700

    disable flit

--- neovim/nvim/lua/vscode_lua/plugins/leap.lua
+++ neovim/nvim/lua/vscode_lua/plugins/leap.lua
@@ -5,19 +5,19 @@ return {
     { "folke/flash.nvim", enabled = false, optional = true },
   
     -- easily jump to any location and enhanced f/t motions for Leap
-    {
-      "ggandor/flit.nvim",
-      enabled = true,
-      keys = function()
-        ---@type LazyKeys[]
-        local ret = {}
-        for _, key in ipairs({ "f", "F", "t", "T" }) do
-          ret[#ret + 1] = { key, mode = { "n", "x", "o" }, desc = key }
-        end
-        return ret
-      end,
-      opts = { labeled_modes = "nx" },
-    },
+    -- {
+    --   "ggandor/flit.nvim",
+    --   enabled = true,
+    --   keys = function()
+    --     ---@type LazyKeys[]
+    --     local ret = {}
+    --     for _, key in ipairs({ "f", "F", "t", "T" }) do
+    --       ret[#ret + 1] = { key, mode = { "n", "x", "o" }, desc = key }
+    --     end
+    --     return ret
+    --   end,
+    --   opts = { labeled_modes = "nx" },
+    -- },
     {
       "ggandor/leap.nvim",
       enabled = true,
