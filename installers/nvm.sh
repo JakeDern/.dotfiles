5650fc84ad31beaf09fbe74f709f1d2db0d8e41c
Jake Dern <jakedern@microsoft.com>
2023-09-18 08:43:42 -0700

    add nvm commands

--- installers/nvm.sh
+++ installers/nvm.sh
@@ -6,3 +6,6 @@ if ! command -v nvm &>/dev/null; then
 	echo "Installing nvm"
 	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
 fi
+
+echo "Try: nvm install --lts"
+echo "Try: nvm use --lts"
