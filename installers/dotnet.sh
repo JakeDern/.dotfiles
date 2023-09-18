21c2272f9d3d641f801e91a722cece37309c54a2
Jake Dern <jakedern@microsoft.com>
2023-09-18 09:31:49 -0700

    dotnet

---
+++ installers/dotnet.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Check if dotnet is installed and install it if not
+
+
+# Add microsoft source
+wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb > /dev/null 2>&1
+sudo dpkg -i packages-microsoft-prod.deb > /dev/null 2>&1
+rm packages-microsoft-prod.deb > /dev/null 2>&1
+
+# Install dotnet
+sudo apt-get update \
+    && sudo apt-get install -y dotnet-sdk-7.0 \
+    && sudo apt-get install -y dotnet-sdk-6.0
+
--- installers/nvm.sh
+++ installers/nvm.sh
@@ -2,10 +2,8 @@
 
 # https://github.com/nvm-sh/nvm
 
-if ! command -v nvm &>/dev/null; then
-	echo "Installing nvm"
-	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
-fi
+# Check if nvm is installed using command -v and install it if not
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh > /dev/null 2>&1 | bash
 
 echo "Try: nvm install --lts"
 echo "Try: nvm use --lts"
