97f37ae6b74768e0cbabe19d1ec2275f84530fac
Jake Dern <jakedern@microsoft.com>
2023-10-21 10:57:58 -0700

    installers

--- installers/ado-cred-provider.sh
+++ installers/ado-cred-provider.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# https://github.com/microsoft/artifacts-credprovider#setup
+# https://github.com/open-telemetry/opentelemetry-collector/tree/main/cmd/builder
 
 wget -qO- https://aka.ms/install-artifacts-credprovider.sh | bash
--- installers/go.sh
+++ installers/go.sh
@@ -3,7 +3,7 @@
 # https://go.dev/dl/
 # https://go.dev/doc/install
 
-GO_VERSION="1.21.1"
+GO_VERSION="1.20"
 DOWNLOAD_DIR=`mktemp -d`
 wget -o- "https://go.dev/dl/go${GO_VERSION}.linux-amd64.tar.gz" -P $DOWNLOAD_DIR
 rm -rf /usr/local/go && tar -C /usr/local -xzf $DOWNLOAD_DIR/go$GO_VERSION.linux-amd64.tar.gz
