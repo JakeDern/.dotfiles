9bd4c9ab9ad3ddfb88b57a57c0b85af462fae91a
Jake Dern <jakedern@microsoft.com>
2023-10-22 13:12:49 -0700

    add fzf install

--- neovim/nvim/lua/normal/plugins/telescope.lua
+++ neovim/nvim/lua/normal/plugins/telescope.lua
@@ -1,6 +1,9 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.4',
-      dependencies = { 'nvim-lua/plenary.nvim' }
-    }
+    {
+        'nvim-telescope/telescope.nvim', tag = '0.1.4',
+          dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+}
 
 
