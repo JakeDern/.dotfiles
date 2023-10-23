9c0b3321389c4dd59281dd880b244e342ea2cca4
Jake Dern <jakedern@microsoft.com>
2023-10-22 17:08:37 -0700

    comment

--- neovim/nvim/lua/normal/plugins/leap.lua
+++ neovim/nvim/lua/normal/plugins/leap.lua
@@ -1,7 +1,12 @@
 local setup = function()
     local leap = require('leap')
     leap.add_default_mappings()
+
+    -- There's some race condition with highlight colors that this
+    -- solves
     leap.init_highlight(true)
+
+    -- Set the highlight groups
     vim.api.nvim_set_hl(0, 'LeapBackdrop', { link = 'Comment' })
     vim.api.nvim_set_hl(0, 'LeapMatch', {
       -- For light themes, set to 'black' or similar.
