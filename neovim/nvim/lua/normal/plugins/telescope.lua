aee15d601b21b3c46866280866e573e8cec48697
Jake Dern <jakedern@yahoo.com>
2023-11-05 15:20:34 -0800

    layout current buffer find

--- neovim/nvim/lua/normal/plugins/telescope.lua
+++ neovim/nvim/lua/normal/plugins/telescope.lua
@@ -23,7 +23,10 @@ local keymaps = function()
     vim.keymap.set('n', '<leader><leader>', builtin.git_files, {})
     -- Telescope grep_string search=foo 
     -- vim.keymap.set('n', '<leader>fs', builtin.grep_string, {})
-    vim.keymap.set('n', '<C-f>', builtin.current_buffer_fuzzy_find, {})
+    vim.keymap.set('n', '<C-f>', function() builtin.current_buffer_fuzzy_find({
+        sorting_strategy = "ascending",
+        layout_config = {prompt_position = "top"}
+    }) end, {})
 end
 
 return {
