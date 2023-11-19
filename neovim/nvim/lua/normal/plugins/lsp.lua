6fd939c2b7d5cafcf88c09912cdbbd665d652006
Jake Dern <jakedern@yahoo.com>
2023-11-18 16:22:58 -0800

    more lsp structure

--- neovim/nvim/lua/normal/plugins/lsp.lua
+++ neovim/nvim/lua/normal/plugins/lsp.lua
@@ -1,6 +1,19 @@
+-- Configure plugin settings and load extensions here 
+local setup = function()
+   require('lspconfig').gopls.setup({
+    
+   }) 
+end
+
+-- Configure telescope specific keybindings here
+local keymaps = function()
+    
+end
+
 return {
     {
       -- LSP Configuration & Plugins
+      -- basically pre-baked configurations for many lsp servers
       'neovim/nvim-lspconfig',
       dependencies = {
           -- Automatically install LSPs to stdpath for neovim
@@ -10,5 +23,9 @@ return {
           -- Automatically configures lua language server for your neovim config
           { 'folke/neodev.nvim', opts = {} },
     },
+    config = function()
+        setup()
+        keymaps()
+    end,
   },
 }
