d2fffc01cfca804ab32c2af9fdaa8d03abb6ede4
Jake Dern <jakedern@microsoft.com>
2023-11-22 11:29:54 -0800

    nvim updates

--- neovim/nvim/lua/normal/plugins/nvim-cmp.lua
+++ neovim/nvim/lua/normal/plugins/nvim-cmp.lua
@@ -1,4 +1,24 @@
 -- todo
+local setup = function()
+
+end
+
+local keymaps = function()
+
+end
+
 return {
-    'hrsh7th/nvim-cmp'
+    {
+     'hrsh7th/nvim-cmp',
+      dependencies = {
+          -- Completion sources
+          'hrsh7th/cmp-nvim-lsp',
+          'hrsh7th/cmp-buffer',
+          'hrsh7th/cmp-path',
+    },
+    config = function()
+        setup()
+        keymaps()
+    end,
+  },
 }
