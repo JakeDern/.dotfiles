d845492203eeeca4e6bdb2c73c911d765864aa9d
Jake Dern <jakedern@microsoft.com>
2023-11-22 12:00:26 -0800

    rename

---
+++ neovim/nvim/lua/normal/plugins/completions.lua
@@ -0,0 +1,73 @@
+local setup = function()
+    local cmp = require('cmp')
+    cmp.setup({
+    snippet = {
+      -- REQUIRED - you must specify a snippet engine
+      expand = function(args)
+        vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+        -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+      end,
+    },
+    window = {
+      -- completion = cmp.config.window.bordered(),
+      -- documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-e>'] = cmp.mapping.abort(),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    }),
+    sources = cmp.config.sources({
+      { name = 'nvim_lsp' },
+      { name = 'vsnip' }, -- For vsnip users.
+      -- { name = 'luasnip' }, -- For luasnip users.
+    }, {
+      { name = 'buffer' },
+    })
+  })
+
+  -- Set configuration for specific filetype.
+  cmp.setup.filetype('gitcommit', {
+    sources = cmp.config.sources({
+      { name = 'git' }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
+    }, {
+      { name = 'buffer' },
+    })
+  })
+
+  -- Set up lspconfig.
+  local capabilities = require('cmp_nvim_lsp').default_capabilities()
+  -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+  require('lspconfig')['<YOUR_LSP_SERVER>'].setup {
+    capabilities = capabilities
+  }
+end
+
+local keymaps = function()
+
+end
+
+return {
+    {
+      -- nvim-cmp is the completion engine   
+      'hrsh7th/nvim-cmp',
+
+      dependencies = {
+          -- These are all of the completion sources
+          'hrsh7th/cmp-nvim-lsp',
+          'hrsh7th/cmp-buffer',
+          'hrsh7th/cmp-path',
+            
+          -- Snippet plugin for actually doing the completion
+          'hrsh7th/vim-vsnip',
+          -- Actually integrates the snipped plugin with lsp stuff
+          'hrsh7th/vim-vsnip-integ'
+    },
+    config = function()
+        setup()
+        keymaps()
+    end,
+  },
+}
