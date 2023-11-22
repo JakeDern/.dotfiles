3b06fbd96d4dca94765360817717ba3883d04d26
Jake Dern <jakedern@microsoft.com>
2023-11-22 11:44:58 -0800

    more plugins for completion

--- neovim/nvim/lua/normal/plugins/nvim-cmp.lua
+++ neovim/nvim/lua/normal/plugins/nvim-cmp.lua
@@ -17,6 +17,11 @@ return {
           'hrsh7th/cmp-nvim-lsp',
           'hrsh7th/cmp-buffer',
           'hrsh7th/cmp-path',
+            
+          -- Snippet plugin for actually doing the completion
+          'hrsh7th/vim-vsnip',
+          -- Actually integrates the snipped plugin with lsp stuff
+          'hrsh7th/vim-vsnip-integ'
     },
     config = function()
         setup()
