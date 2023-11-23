528894d4b6e8d6e6eba874288191a99fe83d5117
Jake Dern <jakedern@microsoft.com>
2023-11-23 10:48:35 -0800

    cleanup

--- neovim/nvim/lua/normal/plugins/completions.lua
+++ neovim/nvim/lua/normal/plugins/completions.lua
@@ -4,7 +4,6 @@ local setup = function()
     snippet = {
       -- REQUIRED - you must specify a snippet engine
       expand = function(args)
-        -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
         require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
       end,
     },
@@ -21,17 +20,7 @@ local setup = function()
     }),
     sources = cmp.config.sources({
       { name = 'nvim_lsp' },
-      -- { name = 'vsnip' }, -- For vsnip users.
       { name = 'luasnip' }, -- For luasnip users.
-    }, {
-      { name = 'buffer' },
-    })
-  })
-
-  -- Set configuration for specific filetype.
-  cmp.setup.filetype('gitcommit', {
-    sources = cmp.config.sources({
-      { name = 'git' }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
     }, {
       { name = 'buffer' },
     })
@@ -39,7 +28,7 @@ local setup = function()
 
   -- Set up lspconfig.
   local capabilities = require('cmp_nvim_lsp').default_capabilities()
-  -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+  -- Add an entry for every lsp server in here
   require('lspconfig')['gopls'].setup {
     capabilities = capabilities
   }
@@ -61,10 +50,8 @@ return {
           'hrsh7th/cmp-path',
             
           -- Snippet plugin for actually doing the completion
-          -- 'hrsh7th/vim-vsnip',
-          -- Actually integrates the snipped plugin with lsp stuff
-          -- 'hrsh7th/vim-vsnip-integ'
           'L3MON4D3/LuaSnip',
+          -- Actually integrates the snippet plugin with lsp stuff
           'saadparwaiz1/cmp_luasnip',
     },
     config = function()
