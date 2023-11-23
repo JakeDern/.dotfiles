61465c15394371987eafab4163242774ab8d1770
Jake Dern <jakedern@microsoft.com>
2023-11-23 10:34:07 -0800

    remove mason stuff since it's not being used

--- neovim/nvim/lua/normal/plugins/lsp.lua
+++ neovim/nvim/lua/normal/plugins/lsp.lua
@@ -43,11 +43,13 @@ return {
       'neovim/nvim-lspconfig',
       dependencies = {
           -- Automatically install LSPs to stdpath for neovim
-          'williamboman/mason.nvim',
-          'williamboman/mason-lspconfig.nvim',
+          -- TODO
+          -- 'williamboman/mason.nvim',
+          -- 'williamboman/mason-lspconfig.nvim',
 
           -- Automatically configures lua language server for your neovim config
-          { 'folke/neodev.nvim', opts = {} },
+          -- TODO
+          -- { 'folke/neodev.nvim', opts = {} },
     },
     config = function()
         setup()
