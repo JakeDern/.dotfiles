b216db56e1a452cba00bca5f2080f19d3e15d513
Jake Dern <jakedern@microsoft.com>
2023-11-23 13:55:06 -0800

    treesitter

--- neovim/nvim/lua/normal/plugins/treesitter.lua
+++ neovim/nvim/lua/normal/plugins/treesitter.lua
@@ -7,23 +7,13 @@ return {
     "nvim-treesitter/nvim-treesitter",
     version = false, -- last release is way too old and doesn't work on Windows
     build = ":TSUpdate",
-    event = { "LazyFile", "VeryLazy" },
-    init = function(plugin)
-      -- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
-      -- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
-      -- no longer trigger the **nvim-treeitter** module to be loaded in time.
-      -- Luckily, the only thins that those plugins need are the custom queries, which we make available
-      -- during startup.
-      require("lazy.core.loader").add_to_rtp(plugin)
-      require("nvim-treesitter.query_predicates")
-    end,
+    event = { "VeryLazy" },
     dependencies = {
       {
         "nvim-treesitter/nvim-treesitter-textobjects",
       },
     },
     cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
-
     opts = {
       highlight = { enable = true },
       indent = { enable = true },
@@ -51,21 +41,12 @@ return {
         -- "vimdoc",
         "yaml",
         "go",
-        "csharp",
-      },
+        "c_sharp",
       },
     },
     ---@param opts TSConfig
     config = function(_, opts)
       require("nvim-treesitter.configs").setup(opts)
     end,
-  },
-
-  -- Show context of the current function
-  {
-    "nvim-treesitter/nvim-treesitter-context",
-    event = "LazyFile",
-    enabled = true,
-    opts = { mode = "cursor", max_lines = 3 },
   },
 }
