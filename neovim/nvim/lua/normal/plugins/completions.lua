85e92b16b8891a84b9270cf6a7193a527120a1eb
Jake Dern <jakedern@microsoft.com>
2023-11-23 12:25:39 -0800

    next/prev for completions

--- neovim/nvim/lua/normal/plugins/completions.lua
+++ neovim/nvim/lua/normal/plugins/completions.lua
@@ -16,6 +16,8 @@ local setup = function()
       ['<C-f>'] = cmp.mapping.scroll_docs(4),
       ['<C-Space>'] = cmp.mapping.complete(),
       ['<C-e>'] = cmp.mapping.abort(),
+      ['<C-j>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+      ['<C-k>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
       ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     }),
     sources = cmp.config.sources({
