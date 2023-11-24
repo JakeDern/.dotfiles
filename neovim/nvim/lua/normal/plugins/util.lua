00c37d25068b30180eb531222603cbe83abb6e37
Jake Dern <jakedern@microsoft.com>
2023-11-24 09:35:12 -0800

    comment mappings

--- neovim/nvim/lua/normal/plugins/util.lua
+++ neovim/nvim/lua/normal/plugins/util.lua
@@ -6,6 +6,19 @@ return {
     },
     config = function()
         require('Comment').setup()
+
+        -- Maps Ctrl+/ to toggle single line comments
+        -- Ctrl+/ is interpreted as <C-_> by Neovim
+        vim.keymap.set("n", "<C-_>", require('Comment.api').toggle.linewise.current, { noremap = true, silent = true })
+
+        -- Maps Ctrl+/ to toggle multiple selected linewise
+        -- This was taken from the Comments.Nvim api code
+        -- https://github.com/numToStr/Comment.nvim/blob/master/lua/Comment/api.lua
+        local esc = vim.api.nvim_replace_termcodes('<ESC>', true, false, true) 
+        vim.keymap.set('x', '<C-_>', function()
+            vim.api.nvim_feedkeys(esc, 'nx', false)
+            require('Comment.api').toggle.linewise(vim.fn.visualmode())
+        end)
     end,
     lazy = false,
 }
