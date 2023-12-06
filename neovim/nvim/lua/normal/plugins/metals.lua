b39315203945f7d22fd9e41a3673eaa71a8271f4
Jake Dern <jakedern@microsoft.com>
2023-12-05 16:27:16 -0800

    checkpoint

--- neovim/nvim/lua/normal/plugins/metals.lua
+++ neovim/nvim/lua/normal/plugins/metals.lua
@@ -5,36 +5,7 @@ return {
       "nvim-lua/plenary.nvim",
       "hrsh7th/cmp-nvim-lsp",
       'VonHeikemen/lsp-zero.nvim',
-    -- TODO: Setup dap stuff at some point 
-    --   {
-    --     "mfussenegger/nvim-dap",
-    --     config = function(self, opts)
-    --       -- Debug settings if you're using nvim-dap
-    --       local dap = require("dap")
-    --
-    --       dap.configurations.scala = {
-    --         {
-    --           type = "scala",
-    --           request = "launch",
-    --           name = "RunOrTest",
-    --           metals = {
-    --             runType = "runOrTestFile",
-    --             --args = { "firstArg", "secondArg", "thirdArg" }, -- here just as an example
-    --           },
-    --         },
-    --         {
-    --           type = "scala",
-    --           request = "launch",
-    --           name = "Test Target",
-    --           metals = {
-    --             runType = "testTarget",
-    --           },
-    --         },
-    --       }
-    --     end
-    --   },
-    -- },
-    ft = { "scala", "sbt", "java" },
+    },
     opts = function()
         local metals_config = require("metals").bare_config()
 
@@ -73,17 +44,15 @@ return {
         end
         return metals_config
       end,
-      config = function(self, metals_config)
-        local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
-        vim.api.nvim_create_autocmd("FileType", {
-          pattern = self.ft,
-          callback = function()
-            require("metals").initialize_or_attach(metals_config)
-          end,
-          group = nvim_metals_group,
-        })
-      end
-
-    }
+    config = function(self, metals_config)
+      local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = self.ft,
+        callback = function()
+          require("metals").initialize_or_attach(metals_config)
+        end,
+        group = nvim_metals_group,
+      })
+    end
   }
 }
