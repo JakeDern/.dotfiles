ef9fe9631d6a03acafb2fc6590f36c960a2d1389
Jake Dern <jakedern@microsoft.com>
2023-12-30 17:35:08 -0800

    default zellij layout

--- terminal/setup.sh
+++ terminal/setup.sh
@@ -6,3 +6,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 mkdir -p ~/.config/zellij
 ln -rsf $SCRIPT_DIR/config.kdl ~/.config/zellij/config.kdl # r for relative path, s for symbolic, f for force
 
+# Install zellij layout file
+mkdir -p ~/.config/zellij/layouts
+ln -rsf $SCRIPT_DIR/default_layout.kdl ~/.config/zellij/layouts/default.kdl # r for relative path, s for symbolic, f for force
+
