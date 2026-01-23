d0fb91c8f03a5cf15897b1d63a46c5aa209e0dca
Jake Dern <jakedern@yahoo.com>
2026-01-22 19:11:05 -0800

    Migrate more stuff away from lsp-zero

--- neovim/nvim/lua/custom/lsp-zero.lua
+++ neovim/nvim/lua/custom/lsp-zero.lua
@@ -1,38 +1,3 @@
-local key_set = function(mode, key, func, bufnr)
-  vim.keymap.set(mode, key, func, { buffer = bufnr })
-end
-
-local override_keymaps = {
-}
-
-local apply_keymaps = function(_, bufnr)
-  local fzf = require('fzf-lua')
-  key_set("n", "gt", vim.lsp.buf.type_definition, bufnr)
-  key_set("n", "K", vim.lsp.buf.hover, bufnr)
-  key_set("n", "<leader>r", vim.lsp.buf.rename, bufnr)
-  key_set("n", "<leader>.", vim.lsp.buf.code_action, bufnr)
-
-  -- Diagnostics
-  key_set("n", "<leader>dj", function()
-    vim.diagnostic.jump({ count = 1 })
-  end, bufnr)
-  key_set("n", "<leader>dk", function()
-    vim.diagnostic.jump({ count = -1 })
-  end, bufnr)
-  key_set("n", "<leader>dh", vim.diagnostic.open_float, bufnr)
-  -- TODO: Configure this in FZF
-  -- key_set("n", "<leader>dl", telescope.diagnostics, bufnr)
-  key_set("n", "<leader>dl", fzf.diagnostics_document, bufnr)
-  key_set("n", "<leader>dw", fzf.diagnostics_workspace, bufnr)
-
-  -- References, definitions
-  -- key_set("n", "gr", telescope.lsp_references, bufnr)
-  key_set("n", "gr", fzf.lsp_references, bufnr)
-  key_set("n", "gd", vim.lsp.buf.definition, bufnr)
-  -- key_set("n", "gi", telescope.lsp_implementations, bufnr)
-  key_set("n", "gi", fzf.lsp_implementations, bufnr)
-end
-
 -- For some reason bicepparam files are not properly detected
 vim.filetype.add({
   extension = {
@@ -45,59 +10,12 @@ local lsp_zero = require('lsp-zero')
 -- This configures the on_attach function for lsp-zero, this function
 -- will be called when lsp_zero calls lsp-config to do setup
 lsp_zero.on_attach(function(client, bufnr)
-  apply_keymaps(client, bufnr)
-  local overrides = override_keymaps[client.name]
-  if overrides then
-    overrides(client, bufnr)
-  end
-
-  -- Some day should try the other formatting options:
-  -- https://lsp-zero.netlify.app/v3.x/reference/lua-api.html#async-autoformat-client-bufnr-opts
-  -- https://lsp-zero.netlify.app/v3.x/reference/lua-api.html#format-on-save-opts
-  lsp_zero.buffer_autoformat()
-
-  -- Organize imports on save if the client supports it
-  local augroup = vim.api.nvim_create_augroup("JD_LSP", { clear = true })
-
   -- Enable inlay hints
   if vim.lsp.inlay_hint then
     vim.lsp.inlay_hint.enable(false, { 0 })
   end
-
-  if client.supports_method("source.organize_imports_on_format") then
-    vim.api.nvim_create_autocmd('BufWritePre', {
-      group = augroup,
-      desc = ("Organize imports for '%s'"):format(client.name),
-      pattern = { "*.go", "*.mod", "*.sum" },
-
-      -- Implementation copied from Go docs:
-      -- https://github.com/golang/tools/blob/master/gopls/doc/vim.md#imports-and-formatting
-      callback = function(_)
-        local params = vim.lsp.util.make_range_params()
-        params.context = { only = { "source.organizeImports" } }
-        local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
-        for cid, res in pairs(result or {}) do
-          for _, r in pairs(res.result or {}) do
-            if r.edit then
-              local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
-              vim.lsp.util.apply_workspace_edit(r.edit, enc)
-            end
-          end
-        end
-      end,
-    })
-  end
 end)
 
-
--- Technically these are "diagnostic signs", neovim enables them by default.
--- Here we are just changing them to fancy icons.
-lsp_zero.set_sign_icons({
-  error = '✘',
-  warn = '▲',
-  hint = '⚑',
-  info = '»'
-})
 
 -- Mason docs state to set up 'mason' before 'mason-lspconfig'
 -- There isn't really any configuration for mason except for
--- neovim/nvim/plugin/lsp.lua
+++ neovim/nvim/plugin/lsp.lua
@@ -12,3 +12,77 @@ local lsps = {
 for _, lsp in ipairs(lsps) do
   vim.lsp.enable(lsp)
 end
+
+
+local key_set = function(mode, key, func, bufnr)
+  vim.keymap.set(mode, key, func, { buffer = bufnr })
+end
+
+-- Set the symbols for diagnostics in the lane
+local ds = vim.diagnostic.severity
+vim.diagnostic.config({
+  signs = {
+    text = {
+      [ds.ERROR] = '✘',
+      [ds.WARN] = '▲',
+      [ds.INFO] = '»',
+      [ds.HINT] = '⚑'
+    }
+  }
+})
+
+
+-- Main lsp attach function. Sets up all the keybindings and some augroups
+-- that do stuff like formatting
+local group = vim.api.nvim_create_augroup('jakedern.lsp', {})
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = group,
+  callback = function(args)
+    print("Setting up keybindings!")
+    local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+    print(client.name)
+
+    -- TODO: Add all keybindings here
+    local bufnr = args.buf
+    local fzf = require('fzf-lua')
+    key_set("n", "gt", vim.lsp.buf.type_definition, bufnr)
+    key_set("n", "K", vim.lsp.buf.hover, bufnr)
+    key_set("n", "<leader>r", vim.lsp.buf.rename, bufnr)
+    key_set("n", "<leader>.", vim.lsp.buf.code_action, bufnr)
+
+    -- Diagnostics
+    key_set("n", "<leader>dj", function()
+      vim.diagnostic.jump({ count = 1 })
+    end, bufnr)
+    key_set("n", "<leader>dk", function()
+      vim.diagnostic.jump({ count = -1 })
+    end, bufnr)
+    key_set("n", "<leader>dh", vim.diagnostic.open_float, bufnr)
+    key_set("n", "<leader>dl", fzf.diagnostics_document, bufnr)
+    key_set("n", "<leader>dw", fzf.diagnostics_workspace, bufnr)
+
+    -- References, definitions
+    key_set("n", "gr", fzf.lsp_references, bufnr)
+    key_set("n", "gd", vim.lsp.buf.definition, bufnr)
+    key_set("n", "gi", fzf.lsp_implementations, bufnr)
+
+
+    -- TODO: See if the below will work with gopls that seems to have it's own
+    -- special page on organize imports:
+    --    https://github.com/golang/tools/blob/master/gopls/doc/vim.md#imports-and-formatting
+
+
+    -- Auto-format ("lint") on save.
+    -- Usually not needed if server supports "textDocument/willSaveWaitUntil".
+    if not client:supports_method('textDocument/willSaveWaitUntil')
+        and client:supports_method('textDocument/formatting') then
+      vim.api.nvim_create_autocmd('BufWritePre', {
+        group = group,
+        buffer = args.buf,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
+        end,
+      })
+    end
+  end,
+})
